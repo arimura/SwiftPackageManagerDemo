e6afb87ac1a65a0e6aab5587108c13c8383cf034
arimura <33759663+arimura@users.noreply.github.com>
2023-12-20 17:21:56 +0900

    Add

---
+++ SwiftPackageManagerDemo/SwiftPackageManagerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPackageManagerDemo
+//
+//  Created by Kotaro Arimura on 2023/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
